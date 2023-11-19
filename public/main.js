9a29459b73b04f529196f0726c8d66e6158ddd67
Nitin sk <nitinskolekar@gmail.com>
2023-11-19 15:08:27 +0530

    finalchange

--- public/main.js
+++ public/main.js
@@ -1,40 +1,16 @@
-// Apply fading effect to sections on scroll
 const sections = document.querySelectorAll('section');
+sections.forEach(section => section.classList.add('show'));
 
-function checkSections() {
-    sections.forEach((section) => {
-        const sectionTop = section.getBoundingClientRect().top;
-        if (sectionTop < window.innerHeight - 100) {
-            section.classList.add('show');
-        }
-    });
-}
-
-window.addEventListener('scroll', checkSections);
-
-function revealOnScroll() {
-    const sections = document.querySelectorAll('.scroll-reveal');
-
-    sections.forEach(section => {
-        const sectionTop = section.getBoundingClientRect().top;
-        const windowHeight = window.innerHeight;
-
-        if (sectionTop < windowHeight / 1.2) {
-            section.classList.add('revealed');
-        }
-    });
-}
 
 document.getElementById("contact-button").addEventListener("click", function () {
-    var email = "nitinskolekar@email.com"; // Replace with your email address
-    var subject = "Regarding your portfolio"; // Replace with the email subject
-    var body = "Any Queries ?"; // Replace with the email body
+    var email = "nitinskolekar@email.com";
+    var subject = "Regarding your portfolio";
+    var body = "Any Queries ?";
 
     var mailtoLink = "mailto:" + email + "?subject=" + subject + "&body=" + body;
     window.location.href = mailtoLink;
 });
 
-// Add this code to update the language using AJAX
 document.querySelector("#language-toggle").addEventListener("click", function () {
     const currentLanguage = document.documentElement.lang === "en" ? "ja" : "en";
 
@@ -42,7 +18,7 @@ document.querySelector("#language-toggle").addEventListener("click", function ()
         .then((response) => response.json())
         .then((data) => {
             if (data.success) {
-                location.reload(); // Refresh the page after language change
+                location.reload();
             }
         });
 });
